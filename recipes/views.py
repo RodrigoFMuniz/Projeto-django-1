9bcc090288c31194704683bab17ec08550f0e61c
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-02-06 01:11:44 -0300

    Adicionado pacote Faker para inserção e testes dos templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,23 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
     obj = {
-        'nome': 'Rodrigo',
-        'sobrenome': 'Muniz',
-        'age': 35,
+        'recipes': [make_recipe() for _ in range(10)],
     }
     return HttpResponse(render_to_string('recipes\\pages\\home.html', context=obj))
 
 
 def recipe(request, id):
     obj = {
-        'nome': 'Rodrigo',
-        'sobrenome': 'Muniz',
-        'age': 35,
+        'recipe': make_recipe(),
     }
     return render(request, 'recipes\\pages\\recipe.html', context=obj)
