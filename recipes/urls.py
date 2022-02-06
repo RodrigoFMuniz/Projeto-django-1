92315d2f41e85108da3ed97d33b77254aeb151eb
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-02-05 22:15:33 -0300

    retirada app teste

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('recipe/<int:id>/', views.recipe),
-    path('teste/', include('teste.urls'))
 ]
--- teste/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template.loader import render_to_string
-
-
-# Create your views here.
-def teste(request):
-    return HttpResponse(render_to_string('teste\\teste.html'))
