340bb02819d01c736393844e6204f64c91626479
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-02-06 12:35:45 -0300

    Adiocnando dinamismo as URLs dos links da lista

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,12 @@
+from unicodedata import name
+
 from django.urls import include, path
 
 from . import views
 
+app_name = 'recipes'  # Utilizar recipes:recipe, ao invés de recipes-recipe
+
 urlpatterns = [
-    path('', views.home),
-    path('recipe/<int:id>/', views.recipe),
+    path('', views.home, name='home'),
+    path('recipe/<int:id>/', views.recipe, name="recipe"),
 ]
