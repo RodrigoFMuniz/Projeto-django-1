ee94acae9e2e54553c1b215b4003535a38f2e06b
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-02-08 10:38:21 -0300

    Finalizado o model para recipes. Push pré migration

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,32 @@
+from distutils.command.upload import upload
+from email.policy import default
+
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+# class Author(models.Model):
+#     name = models.CharField(65)
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=150)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()  # campo indexado
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
