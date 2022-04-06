7f6f168658a2b144241768463784448ff7bc8049
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-06 15:03:59 -0300

    updated settings

--- myfirstdjangoproject1/settings.py
+++ myfirstdjangoproject1/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 print("Diratório raiz", BASE_DIR)
@@ -21,7 +23,7 @@ print("Diratório raiz", BASE_DIR)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sm#r!oep&kcym+!!dd#c$wpldrgj$1#$##1@5_+%i$64z+214y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
