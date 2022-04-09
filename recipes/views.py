83dc17f82fb526dce832f2545bf48a3d4d7b0306
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-08 23:35:27 -0300

    Using get_list_or_404 method

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,5 @@
-from sre_constants import CATEGORY_WORD
-from telnetlib import STATUS
-
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.template.loader import render_to_string
 
 # from utils.recipes.factory import make_recipe
@@ -20,15 +17,19 @@ def home(request):
 
 
 def category(request, category_id):
-    recipe = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
-    if not recipe:
-        return render(request, 'recipes\\pages\\404.html', status=404)
-    return render(request, 'recipes\\pages\\categories.html', context={'recipes': recipe, 'title': f'{recipe.first().category.name}'})
+    # recipe = Recipe.objects.filter(
+    #     category__id=category_id, is_published=True).order_by('-id')
+    # if not recipe:
+    #     # return render(request, 'recipes\\pages\\404.html', status=404)
+    #     raise Http404('Error 404 - Categoria não encontrada')
+    recipe = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+    return render(request, 'recipes\\pages\\categories.html', context={'recipes': recipe, 'title': f'{recipe[0].category.name}'})
 
 
 def recipe(request, pk):
     print('id', pk)
     recipe = Recipe.objects.filter(id=pk, is_published=True).first()
     print('recipe', recipe)
+    # recipe = get_object_or_404(Recipe, id=pk, is_published=True)
     return render(request, 'recipes\\pages\\recipe.html', context={'recipe': recipe, 'is_detail_page': True})
